a000898e032ab51dfc58c7b159b36060584d44be
zhaocheng <zhaocheng@westwin.com>
2020-05-29 11:43:55 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo start
 sudo docker pull 542153354/dockerdemo:v1.0 
-sudo docker run --name dockerdemocontainer-it -p 8080:80 542153354/dockerdemo:v1.0 /bin/sh 
-exit 0
+sudo docker run --name dockerdemocontainer -d -p 8080:80 542153354/dockerdemo:v1.0 /bin/sh 
 echo end
 
+exit 0
