8c402f8536ab228f5ebb80d208d17f80b6b2d4a9
zhaocheng <zhaocheng@westwin.com>
2020-05-29 12:47:21 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo start
+containerId="` sudo docker ps | grep "8080->80" | awk  '{print $1}' `"
+echo "containerId:$containerId"
+if [ -z "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+imageId="`sudo docker images | grep "dockerdemo   v1.0" | awk  '{print $3}'`"
+echo "imageId:$imageId"
+#!if [ -z "$imageId" ]
+#!then
+#!	sudo docker rmi  $imageId
+#!fi
+
 sudo docker pull 542153354/dockerdemo:v1.0 
 sudo docker run --name dockerdemocontainer -d -p 8080:80 542153354/dockerdemo:v1.0 /bin/sh 
-echo end
 
-exit 0
+exit
